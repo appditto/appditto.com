fce55694fad7d506d79e4add6329b607b69e013a
yekta <ngyekta@gmail.com>
2019-11-15 03:40:01 +0300

    Removed the emoji

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo/>
       <h1 class="title">appditto.com</h1>
-      <h2 class="subtitle">The website of Appditto 🐵</h2>
+      <h2 class="subtitle">The website of Appditto</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
