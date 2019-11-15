5f7b35ba1857275eae64616db162e01869bc7e1d
yekta <ngyekta@gmail.com>
2019-11-15 03:36:13 +0300

    Format

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,12 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        appditto.com
-      </h1>
-      <h2 class="subtitle">
-        The website of Appditto 🐵
-      </h2>
+      <logo/>
+      <h1 class="title">appditto.com</h1>
+      <h2 class="subtitle">The website of Appditto 🐵</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
