7d76e96794a14182434bfe34c9c2602b90fe77c3
yekta <ngyekta@gmail.com>
2019-11-15 05:38:09 +0300

    Removed eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="container">
     <div>
-      <logo/>
-      <h1 class="title">appditto.com</h1>
-      <h2 class="subtitle">The website of Appditto</h2>
+      <logo />
+      <h1 class="title">
+        Test
+      </h1>
+      <b-row>
+        <b-col md="3"><h1 class="title">Test</h1></b-col>
+        <b-col md="3"><h1 class="title">Test</h1></b-col>
+        <b-col md="3"><h1 class="title">Test</h1></b-col>
+        <b-col md="3"><h1 class="title">Test</h1></b-col>
+      </b-row>
+      <h2 class="subtitle">
+        The website of Appditto
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   </div>
 </template>
