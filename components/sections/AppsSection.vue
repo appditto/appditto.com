e27db1c67f98823c03738d39871e2dfd2d4f0553
yekta <ngyekta@gmail.com>
2019-11-18 18:09:00 +0300

    Turned sections to components

---
+++ components/sections/AppsSection.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="py-5 mt-md-4" id="apps">
+    <b-container fluid class="my-container px-3">
+      <h2 class="w800 text-center">Check out Our Apps</h2>
+      <b-row align-v="center" align-h="center" class="mt-0 mt-md-4">
+        <AppCard
+          website="natrium.io"
+          playStore="play.google.com/store/apps/details?id=co.banano.natriumwallet"
+          appStore="itunes.apple.com/us/app/natrium/id1451425707"
+          appName="Natrium"
+          rating="4.9"
+        />
+        <AppCard
+          website="blaisewallet.com"
+          playStore="play.google.com/store/apps/details?id=com.appditto.blaise"
+          appStore="apps.apple.com/us/app/blaise-pascal-wallet/id1473011216"
+          appName="Blaise"
+          rating="4.8"
+        />
+        <AppCard
+          website="kalium.banano.cc"
+          playStore="play.google.com/store/apps/details?id=com.banano.kaliumwallet&hl=en_US"
+          appStore="itunes.apple.com/us/app/kalium/id1449623414"
+          appName="Kalium"
+          rating="4.5"
+        />
+      </b-row>
+    </b-container>
+    <img
+      src="~/assets/images/testimonials.png"
+      alt="Testimonials"
+      class="w-100 mt-4 d-none d-lg-block"
+    />
+    <img
+      src="~/assets/images/testimonials-md.png"
+      alt="Testimonials Md"
+      class="w-100 mt-4 d-none d-md-block d-lg-none"
+    />
+  </section>
+</template>
+<script>
+import Vue from 'vue'
+import AppCard from '~/components/AppCard.vue'
+export default Vue.extend({
+  name: 'AppsSection',
+  components: {
+    AppCard
+  }
+})
+</script>
---
+++ components/sections/HeroSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="py-4 py-md-0" id="landing">
+    <b-row align-v="center" align-h="center">
+      <b-col cols="12" md="6">
+        <h1 class="text-center text-md-left w800 mt-3">
+          We build <span class="text-primary">beautiful apps</span>,<br
+            class="d-none d-xl-block"
+          />
+          in <span class="text-primary">record time</span>.
+        </h1>
+        <b-row class="px-3 justify-content-center justify-content-md-start">
+          <b-btn
+            href="mailto:hello@appditto.com"
+            variant="primary"
+            size="lg"
+            rounded
+            class="w800 px-5 mt-2 grow-2 btn-shdw-primary"
+            >Let's Talk</b-btn
+          >
+        </b-row>
+      </b-col>
+      <b-col cols="12" md="5" class="mt-4 mt-md-0">
+        <img src="~/assets/images/hero-phones.png" alt="Hero" class="w-100" />
+      </b-col>
+    </b-row>
+  </section>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HeroSection'
+})
+</script>
