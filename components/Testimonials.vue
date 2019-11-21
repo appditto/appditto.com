6ddebb2904c20ef93618d2ac978bcec03abb5fa5
yekta <ngyekta@gmail.com>
2019-11-21 12:49:02 +0300

    Testimonial svg improvements

---
+++ components/Testimonials.vue
@@ -0,0 +1,356 @@
+<template>
+  <svg
+    class="w-100 mt-4 d-none d-lg-block"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1440 347"
+  >
+    <title>Artboard 1</title>
+    <path
+      d="M1034,210.5a10.5,10.5,0,0,1,10.5-10.5h204a10.5,10.5,0,0,1,10.5,10.5v89a10.5,10.5,0,0,1-10.5,10.5H1060.66l-21.86,16.4a3,3,0,0,1-4.8-2.4Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M1039.5,203.5V317a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H1252.5a9,9,0,0,0,9-9v-89a9,9,0,0,0-9-9h-204A9,9,0,0,0,1039.5,203.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(1143.215 277)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        James Keillor
+      </text>
+    </g>
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(1066 225)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0009765625em"
+      >
+        Fantastic app, works great
+      </text>
+      <text
+        transform="translate(1066 245)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        and super easy to use.
+      </text>
+    </g>
+    <path
+      d="M828,180.5A10.5,10.5,0,0,1,838.5,170h204a10.5,10.5,0,0,1,10.5,10.5v69a10.5,10.5,0,0,1-10.5,10.5H854.66L832.8,276.4A3,3,0,0,1,828,274Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M833.5,173.5V267a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H1046.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-204A9,9,0,0,0,833.5,173.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(926.547 227)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0010114397321428572em"
+      >
+        Zhao Qing Koh
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(860 195)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Sleek and simple to use.
+      </text>
+    </g>
+    <path
+      d="M310,179.5A10.5,10.5,0,0,1,320.5,169h204A10.5,10.5,0,0,1,535,179.5v69A10.5,10.5,0,0,1,524.5,259H336.66L314.8,275.4A3,3,0,0,1,310,273Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M315.5,172.5V266a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H528.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-204A9,9,0,0,0,315.5,172.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(418.167 226)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Majeed Zoabi
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(342 194)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Sleek and professional!
+      </text>
+    </g>
+    <path
+      d="M404,18.5A10.5,10.5,0,0,1,414.5,8h184A10.5,10.5,0,0,1,609,18.5v69A10.5,10.5,0,0,1,598.5,98H430.66L408.8,114.4A3,3,0,0,1,404,112Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M409.5,11.5V105a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H602.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-184A9,9,0,0,0,409.5,11.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(455.896 65)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Auke van der Meer
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(436 33)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Just perfect.
+      </text>
+    </g>
+    <path
+      d="M634,28.5A10.5,10.5,0,0,1,644.5,18h313A10.5,10.5,0,0,1,968,28.5v87A10.5,10.5,0,0,1,957.5,126H660.66L638.8,142.4A3,3,0,0,1,634,140Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M639.5,21.5V133a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H961.5a9,9,0,0,0,9-9v-87a9,9,0,0,0-9-9h-313A9,9,0,0,0,639.5,21.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(842.133 95)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Jaga My Priera
+      </text>
+    </g>
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(666 43)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Sleek design, easy to use, and well written
+        <tspan
+          x="178.053"
+          y="0"
+          style="letter-spacing:-0.025960286458333332em"
+        ></tspan>
+      </text>
+      <text
+        transform="translate(666 63)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.011962890625em"
+      >
+        code. Oh, and I love the paper wallet feauture.
+      </text>
+    </g>
+    <path
+      d="M1230,160.5a10.5,10.5,0,0,1,10.5-10.5h204a10.5,10.5,0,0,1,10.5,10.5v69a10.5,10.5,0,0,1-10.5,10.5H1256.66l-21.86,16.4a3,3,0,0,1-4.8-2.4Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M1235.5,153.5V247a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H1448.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-204A9,9,0,0,0,1235.5,153.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(1334.933 207)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Bryan Lemyre
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(1262 175)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0009765625em"
+      >
+        Great, fast, always works.
+      </text>
+    </g>
+    <path
+      d="M992,21.5A10.5,10.5,0,0,1,1002.5,11h354A10.5,10.5,0,0,1,1367,21.5v127a10.5,10.5,0,0,1-10.5,10.5H1019.635l-22.876,16.556a3,3,0,0,1-4.759-2.43Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M997.5,14.5V166.126a1.5,1.5,0,0,0,2.379,1.215l23.27-16.841H1360.5a9,9,0,0,0,9-9V14.5a9,9,0,0,0-9-9h-354A9,9,0,0,0,997.5,14.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(1024 36)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Natrium is easily the most robust and intuitive Nano
+      </text>
+      <text
+        transform="translate(1024 56)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        wallet. It gives the most control over your wallet’s
+      </text>
+      <text
+        transform="translate(1024 76)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0009765625em"
+      >
+        security, contacts, and representative choice. Sending
+      </text>
+      <text
+        transform="translate(1024 96)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        and receiving couldn’t be easier. Highly recommended.
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(1271.846 128)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0010114397321428572em"
+      >
+        Nate Ryan
+      </text>
+    </g>
+    <path
+      d="M551,228.5A10.5,10.5,0,0,1,561.5,218h276A10.5,10.5,0,0,1,848,228.5v87A10.5,10.5,0,0,1,837.5,326H578.635l-22.876,16.556a3,3,0,0,1-4.759-2.43Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M556.5,221.5V333.126a1.5,1.5,0,0,0,2.379,1.215l23.27-16.841H841.5a9,9,0,0,0,9-9v-87a9,9,0,0,0-9-9h-276A9,9,0,0,0,556.5,221.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(698.476 295)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Sonel Prabhakaran
+      </text>
+    </g>
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(583 243)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0009765625em"
+      >
+        Fast, reliable, easy to use and looks
+      </text>
+      <text
+        transform="translate(583 263)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        great. What more could you want?
+      </text>
+    </g>
+    <path
+      d="M488,93.5A10.5,10.5,0,0,1,498.5,83h296A10.5,10.5,0,0,1,805,93.5v83A10.5,10.5,0,0,1,794.5,187H515.635l-22.876,16.556a3,3,0,0,1-4.759-2.43Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M493.5,86.5V194.126a1.5,1.5,0,0,0,2.379,1.215l23.27-16.841H798.5a9,9,0,0,0,9-9v-83a9,9,0,0,0-9-9h-296A9,9,0,0,0,493.5,86.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(672.413 160)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Shivang Saxena
+      </text>
+    </g>
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(520 108)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500"
+      >
+        Amazing UI, great security, push notifications,
+      </text>
+      <text
+        transform="translate(520 128)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500"
+      >
+        I’m not sure I could ask for anything else.
+      </text>
+    </g>
+    <path
+      d="M-25,167.5A10.5,10.5,0,0,1-14.5,157h294A10.5,10.5,0,0,1,290,167.5v69A10.5,10.5,0,0,1,279.5,247H1.66L-20.2,263.4a3,3,0,0,1-4.2-.6A3,3,0,0,1-25,261Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M-19.5,160.5V254a1.5,1.5,0,0,0,.3.9,1.5,1.5,0,0,0,2.1.3L5.16,238.5H283.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-294A9,9,0,0,0-19.5,160.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(186.747 214)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0010114397321428572em"
+      >
+        Dalibor Rile
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(7 182)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.001013020833333336em"
+      >
+        Works great, easy and fast, excellent design!
+      </text>
+    </g>
+    <path
+      d="M75,42.5A10.5,10.5,0,0,1,85.5,32h321A10.5,10.5,0,0,1,417,42.5v89A10.5,10.5,0,0,1,406.5,142H101.66L79.8,158.4A3,3,0,0,1,75,156Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M80.5,35.5V149a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H410.5a9,9,0,0,0,9-9v-89a9,9,0,0,0-9-9H89.5A9,9,0,0,0,80.5,35.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(289.091 109)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0009765625em"
+      >
+        Andre Görgens
+      </text>
+    </g>
+    <g style="opacity:0.6000000238418579;isolation:isolate">
+      <text
+        transform="translate(107 57)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Really great working wallet. And it looks beautiful,
+      </text>
+      <text
+        transform="translate(107 77)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0009765625em"
+      >
+        love it! Best crypto wallet app I’ve seen so far.
+      </text>
+    </g>
+    <path
+      d="M101,247.5A10.5,10.5,0,0,1,111.5,237h299A10.5,10.5,0,0,1,421,247.5v69A10.5,10.5,0,0,1,410.5,327H127.66L105.8,343.4A3,3,0,0,1,101,341Z"
+      style="fill:#ebf2ff;fill-rule:evenodd"
+    />
+    <path
+      d="M106.5,240.5V334a1.5,1.5,0,0,0,2.4,1.2l22.26-16.7H414.5a9,9,0,0,0,9-9v-69a9,9,0,0,0-9-9h-299A9,9,0,0,0,106.5,240.5Z"
+      style="fill:#fff;stroke:#ebf2ff;stroke-width:3px;fill-rule:evenodd"
+    />
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(300.131 294)"
+        style="isolation:isolate;font-size:14px;fill:#4082ff;font-family:Metropolis-Bold, Metropolis;font-weight:700;letter-spacing:-0.0010114397321428572em"
+      >
+        Felix Dolderer
+      </text>
+    </g>
+    <g style="opacity:0.6;isolation:isolate">
+      <text
+        transform="translate(133 262)"
+        style="isolation:isolate;font-size:12px;fill:#4082ff;font-family:Metropolis-Medium, Metropolis;font-weight:500;letter-spacing:-0.0010172526041666667em"
+      >
+        Stylish, easy to use and speedy performance.
+      </text>
+    </g>
+  </svg>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Testimonials'
+})
+</script>
