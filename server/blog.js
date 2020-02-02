ad7f1dd6bcbddddb417a5a808c1e874519d57ba3
yekta <ngyekta@gmail.com>
2020-02-03 01:22:55 +0300

    Local database and bunch of backend updates nbd :hyperhng:

---
+++ server/blog.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const GhostContentAPI = require('@tryghost/content-api')
+
+// Get the posts from ghost and parse the HTML
+const updateBlogData = async () => {
+    // Create API instance with site credentials
+    const api = new GhostContentAPI({
+        url: 'https://ghost.appditto.com',
+        key: 'd387ba2cdeb5e3b04118440282',
+        version: 'v3'
+    })
+    const imgTagRegex = /<img.*?src="(.*?)"[^\>]+>/g
+    const srcRegex = /src="(.*?)"/g
+    const urlRegex = /(?<=src=")(.*?)(?=")/g
+    const kgImageRegex = /class="kg-image/g
+    const classRegex = /class="(.*?)"/g
+    const cloudinaryUrl100w = 'https://res.cloudinary.com/appditto/image/fetch/w_100,c_limit,q_1,f_auto,pg_1/'
+    const cloudinaryUrl300w = 'https://res.cloudinary.com/appditto/image/fetch/w_300,c_limit,q_80,f_auto/'
+    const cloudinaryUrl600w = 'https://res.cloudinary.com/appditto/image/fetch/w_600,c_limit,q_80,f_auto/'
+    const cloudinaryUrl900w = 'https://res.cloudinary.com/appditto/image/fetch/w_900,c_limit,q_80,f_auto/'
+    const cloudinaryUrl1200w = 'https://res.cloudinary.com/appditto/image/fetch/w_1200,c_limit,q_80,f_auto/'
+    const cloudinaryUrl1500w = 'https://res.cloudinary.com/appditto/image/fetch/w_1500,c_limit,q_80,f_auto/'
+    const cloudinaryUrl2000w = 'https://res.cloudinary.com/appditto/image/fetch/w_2000,c_limit,q_80,f_auto/'
+    await api.posts
+        .browse({
+            limit: 'all',
+        })
+        .then(posts => {
+            posts.forEach(post => {
+                // Edit the html of each single post unless they are policies
+                if (post.feature_image !== undefined && post.html.match(imgTagRegex)) {
+                    // For each img tag in a post
+                    post.html.match(imgTagRegex).forEach(imgTag => {
+                        // Match the image src attribute, including the src=
+                        let imgSrc = imgTag.match(srcRegex)[0];
+                        // Match the url of the src attribute
+                        let imgUrl = imgSrc.match(urlRegex)[0];
+                        // Replace the image src attribute with datasizes, src, srcset and data-srcset
+                        if (imgTag.match(kgImageRegex)) {
+                            post.html = post.html.replace(imgTag, '<div class="image-wrapper">' + imgTag + '</div>')
+                            post.html = post.html.replace(
+                                imgSrc,
+                                `datasizes="auto" src="${cloudinaryUrl1200w + imgUrl}" srcset="${cloudinaryUrl100w + imgUrl}" data-srcset="${cloudinaryUrl300w + imgUrl} 300w, ${cloudinaryUrl600w + imgUrl} 600w, ${cloudinaryUrl900w + imgUrl} 900w, ${cloudinaryUrl1200w + imgUrl} 1200w, ${cloudinaryUrl1500w + imgUrl} 1500w, ${cloudinaryUrl2000w + imgUrl} 2000w"`
+                            )
+                        }
+                    })
+                }
+                // Add lazyloading to items with kg-image class
+                if (post.feature_image !== undefined) { post.html = post.html.replace(kgImageRegex, 'class="kg-image lazyload') }
+            });
+            return posts
+        }).then(posts => {
+            let blogPostsWithoutPolicies = [];
+            posts.forEach(post => {
+                let postData = JSON.stringify(post)
+                fs.writeFile('./blog/posts/' + post.slug + '.json', postData, (err) => {
+                    if (err) throw err;
+                    console.log(`Updated: ${post.slug}, At: ${Date()}, Timestamp: ${Date.now()}`);
+                });
+                if (post.feature_image) {
+                    blogPostsWithoutPolicies.push(post)
+                }
+            })
+            return blogPostsWithoutPolicies
+        }).then(blogPostsWithoutPolicies => {
+            let blogDataWithoutPolicies = JSON.stringify(blogPostsWithoutPolicies)
+            fs.writeFile('./blog/blog.json', blogDataWithoutPolicies, (err) => {
+                if (err) throw err;
+                console.log(`Updated: blog.json, At: ${Date()}, Timestamp: ${Date.now()}`);
+            });
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    return
+}
+
+exports.updateBlogData = updateBlogData
--- server/index.js
+++ server/index.js
@@ -2,34 +2,13 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const GhostContentAPI = require('@tryghost/content-api')
-var mcache = require('memory-cache');
 const fs = require('fs');
-var path = require("path");
+const blog = require('./blog.js');
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 config.unix_socket = process.env.UNIX_SOCKET === 'true'
-
-// For memory cache
-var cache = (duration) => {
-  return (req, res, next) => {
-    let key = '__express__' + req.originalUrl || req.url
-    let cachedBody = mcache.get(key)
-    if (cachedBody) {
-      res.send(cachedBody)
-      return
-    } else {
-      res.sendResponse = res.send
-      res.send = (body) => {
-        mcache.put(key, body, duration * 1000);
-        res.sendResponse(body)
-      }
-      next()
-    }
-  }
-}
 
 
 async function start() {
@@ -46,71 +25,8 @@ async function start() {
     await nuxt.ready()
   }
 
-  // Get the posts from ghost and parse the HTML
-
-  // Create API instance with site credentials
-  const api = new GhostContentAPI({
-    url: 'https://ghost.appditto.com',
-    key: 'd387ba2cdeb5e3b04118440282',
-    version: 'v3'
-  })
-
-  let blogPostsWithoutPolicies = [];
-  let imgTagRegex = /<img.*?src="(.*?)"[^\>]+>/g
-  let srcRegex = /src="(.*?)"/g
-  let urlRegex = /(?<=src=")(.*?)(?=")/g
-  let kgImageRegex = /class="kg-image/g
-  let classRegex = /class="(.*?)"/g
-  let cloudinaryUrl100w = 'https://res.cloudinary.com/appditto/image/fetch/w_100,c_limit,q_1,f_auto,pg_1/'
-  let cloudinaryUrl300w = 'https://res.cloudinary.com/appditto/image/fetch/w_300,c_limit,q_80,f_auto/'
-  let cloudinaryUrl600w = 'https://res.cloudinary.com/appditto/image/fetch/w_600,c_limit,q_80,f_auto/'
-  let cloudinaryUrl900w = 'https://res.cloudinary.com/appditto/image/fetch/w_900,c_limit,q_80,f_auto/'
-  let cloudinaryUrl1200w = 'https://res.cloudinary.com/appditto/image/fetch/w_1200,c_limit,q_80,f_auto/'
-  let cloudinaryUrl1500w = 'https://res.cloudinary.com/appditto/image/fetch/w_1500,c_limit,q_80,f_auto/'
-  let cloudinaryUrl2000w = 'https://res.cloudinary.com/appditto/image/fetch/w_2000,c_limit,q_80,f_auto/'
-  let blogPosts = await api.posts
-    .browse({
-      limit: 'all',
-    })
-    .catch(err => {
-      console.error(err)
-    });
-  // For each post
-  blogPostsWithoutPolicies.forEach(post => {
-    if (post.html.match(imgTagRegex)) {
-      // For each img tag in a post
-      post.html.match(imgTagRegex).forEach(imgTag => {
-        // Match the image src attribute, including the src=
-        let imgSrc = imgTag.match(srcRegex)[0];
-        // Match the url of the src attribute
-        let imgUrl = imgSrc.match(urlRegex)[0];
-        // Replace the image src attribute with datasizes, src, srcset and data-srcset
-        if (imgTag.match(kgImageRegex)) {
-          post.html = post.html.replace(imgTag, '<div class="image-wrapper">' + imgTag + '</div>')
-          post.html = post.html.replace(
-            imgSrc,
-            `datasizes="auto" src="${cloudinaryUrl1200w + imgUrl}" srcset="${cloudinaryUrl100w + imgUrl}" data-srcset="${cloudinaryUrl300w + imgUrl} 300w, ${cloudinaryUrl600w + imgUrl} 600w, ${cloudinaryUrl900w + imgUrl} 900w, ${cloudinaryUrl1200w + imgUrl} 1200w, ${cloudinaryUrl1500w + imgUrl} 1500w, ${cloudinaryUrl2000w + imgUrl} 2000w"`
-          )
-        }
-      })
-    }
-    if (post.feature_image !== undefined) { post.html = post.html.replace(kgImageRegex, 'class="kg-image lazyload') }
-  });
-  blogPosts.forEach(item => {
-    if (item.feature_image) {
-      blogPostsWithoutPolicies.push(item)
-    }
-    let itemData = JSON.stringify(item)
-    fs.writeFile('./blog/posts/' + item.slug + '.json', itemData, (err) => {
-      if (err) throw err;
-      console.log('Data written to file');
-    });
-  });
-  let blogDataWithoutPolicies = JSON.stringify(blogPostsWithoutPolicies)
-  fs.writeFile('./blog/blog.json', blogDataWithoutPolicies, (err) => {
-    if (err) throw err;
-    console.log('Data written to file');
-  });
+  // Update local blog files based on Ghost API
+  blog.updateBlogData()
 
   app.get('/api/ghost/posts', async (req, res) => {
     fs.access('./blog/blog.json', fs.F_OK, (err) => {
